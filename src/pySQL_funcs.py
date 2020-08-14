b3deeef44471c60968abd669fa00e1e29340c2e0
isaac-campbell-smith <icampsmith@gmail.com>
2020-08-13 17:35:06 -0700

    refactored sql funcs to src and added pw string to gitignore

---
+++ src/pySQL_funcs.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def pretty_query(cur, query, conn):
+    conn.rollback()
+    cur.execute(query)
+    data = cur.fetchall() 
+    #cur.description stores SQL column information as a tuple
+        #the name method contains SQL column labels
+    headers = [head.name for head in cur.description]
+    out = pd.DataFrame(data=data, columns=headers)
+    return out
