41e9f5279ddbd24124d1ce177deabadd48033168
isaac-campbell-smith <icampsmith@gmail.com>
2020-10-07 17:37:34 -0700

    finished sql pull script for latest data and added _init_.py

--- src/pull_latest_data.py
+++ src/pull_latest_data.py
@@ -1,12 +1,31 @@
 import pandas as pd
 import psycopg2
+import os
+from pySQL_funcs import pretty_query
 
-conn = psycopg2.connect('dbname=postgres user=postgres host=localhost port=5432 password=password')
-cur = conn.cursor()
-conn.rollback()
-conn.close()
+with open('pw') as pw_file:
+    pw = pw_file.readline()
 
-def pull_load_latest_data():
+def pull_load_latest_data(conn, cur, date):
     """
-    connect to database and write the latest month's data to csv/s
+    Connect to database and write the latest month's data to csv/s
     """
+    tables = ['users', 'battles', 'teammates', 'counters', 
+              'battle_abilities', 'battle_natures', 'battle_items']
+    os.mkdir('../data/latest/'+date)
+    for table in tables:
+        query = """SELECT * FROM {} WHERE month='{}';""".format(table, date)
+        data = pretty_query(cur, query, conn)
+        print (data)
+        write_file = f'../data/latest/{date}/{date}_{table}.csv'
+        data.to_csv(write_file)
+    return
+
+if __name__ == '__main__':
+    date='09-2020'
+    conn = psycopg2.connect(pw)
+    cur = conn.cursor()
+    print (pull_load_latest_data(conn, cur, date))
+    cur.close()
+    conn.close()
+    
